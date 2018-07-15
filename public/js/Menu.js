d32e77e13fd78e74348517c35bd1f64a0eff29ed
Jorge Santamaria <jorsebsant@gmail.com>
2018-07-15 17:13:06 -0500

    feat: webpack working with jsx render and babel

---
+++ public/js/Menu.js
@@ -0,0 +1,6 @@
+import dom from 'jsx-render'
+
+const Menu = () => (
+  <div>heloooo</div>
+)
+export default Menu
---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+import Menu from './Menu'
+
+// client side rendering
+const app = document.body
+app.appendChild(Menu())
