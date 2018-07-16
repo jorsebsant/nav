f1b5f607f02b550f0c0fc8fc2721a86b13688b1e
Jorge Santamaria <jorsebsant@gmail.com>
2018-07-16 16:59:54 -0500

    submenu mobile and event listeners

---
+++ src/components/SubMenu.js
@@ -0,0 +1,15 @@
+import dom from 'jsx-render'
+
+// SubMenu component
+const SubMenu = ({items}) => (  
+  <ul class="submenu">
+    {items.map((item, index) => ( 
+        <li class="submenu__item">
+          <a href={item.url}>
+              {item.title}
+          </a>
+        </li>
+    ))}
+  </ul>
+)
+export default SubMenu
