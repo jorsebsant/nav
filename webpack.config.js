7ab60be7524e2a4ea1d842ba7cc37dbdd90dca18
Jorge Santamaria <jorsebsant@gmail.com>
2018-07-15 21:56:27 -0500

    feat:output files configured from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
 const path = require('path')
 const webpack = require('webpack')
-const { jsxLoader, stylusLoader, cssLoader } = require('./config/loaders')
+const { jsxLoader,stylusLoader,  cssLoader} = require('./config/loaders')
+
 
 
 module.exports = {
-  entry: {
-    App: path.resolve(__dirname, 'src/versions/app.js'),
-  },
+  entry: [
+    path.resolve(__dirname, 'src/versions/app.js'),
+    path.resolve(__dirname, 'src/stylus/main.styl')
+  ],
 
   devtool: 'cheap-source-map',
 
   module: {
-    rules: [jsxLoader, stylusLoader, cssLoader],
+    rules: [
+      jsxLoader,
+      cssLoader,
+      stylusLoader
+    ],
   },
 
   output: {
     path: path.resolve(__dirname, 'public/js'),
-    filename: 'bundle.js',
-    publicPath: '',
-  },  
+  },
 }
